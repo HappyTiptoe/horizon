70088a6b03d765091c6efd7b71451e937171aa33
eb <9453858-eb3n@users.noreply.gitlab.com>
2022-08-18 16:05:48 +0100

    feat: make l messagehandler 1 in 10 chance

--- src/messageHandlers/l.ts
+++ src/messageHandlers/l.ts
@@ -1,8 +1,11 @@
 import { Client, Message } from 'discord.js'
+import { randomChance } from 'tsu'
 import { l } from '../assets/phrases.json'
 
 export function handle(message: Message, client: Client): void {
   if (message.content.replaceAll(/[\s*_]+/g, '').toLowerCase() === 'l') {
-    message.reply(l)
+    if (randomChance(10)) {
+      message.reply(l)
+    }
   }
 }
